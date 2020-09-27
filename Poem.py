797a44231ba5be9b37d81b8cee4ca421366d198b
sebturner01 <sebturner01@gmail.com>
2020-09-27 11:52:57 -0600

    the actual syntax fixes

--- Poem.py
+++ Poem.py
@@ -8,14 +8,14 @@ This class reperesents a poem and the fundemental components contained within it
 #----------------
 # Init 
 #----------------
-import python-Levenshtein
+from Levenshtein import distance
 import fuzzywuzzy
 #----------------
 # Class
 #----------------
 
 class Poem:
-    def __init__(self, textAuthor = "", text = "", PublishedYear : int = 0, attributes):
+    def __init__(self, attributes, textAuthor = "", text = "", PublishedYear = 0, ):
         """Initializes a given Poem as a class."""
         self.author = textAuthor
         self.content = text
@@ -24,20 +24,17 @@ class Poem:
 
     def retrieveAttribute(attribute):
         """Given the name of an attribute as a string this program will return"""
-        if not self.attributes.get(attribute):
+        attr = self.attributes.get(attribute)
+        return attr if attr else fuzzyKeySearch
 
-    def fuzzyKeySearch(key):
-        """This function is to be called by retrieveAttribute in the case that the given
-        attribute was misspelled. Using a levenstien distance of 1 this program will then
-        search the attributes dictionary for the fuzzy match
+    # def fuzzyKeySearch(key):
+    #     """This function is to be called by retrieveAttribute in the case that the given
+    #     attribute was misspelled. Using a levenstien distance of 1 this program will then
+    #     search the attributes dictionary for the fuzzy match
         
-        Input:
-            key - a string that corresponds to a value in "attributes"
+    #     Input:
+    #         key - a string that corresponds to a value in "attributes"
 
-        Output:
-            Returns None if a fuzzy match cannot be found. If there is more than one potential match then both options will be printed out for the user to select.        
-        """
-        
-
-
-    
+    #     Output:
+    #         Returns None if a fuzzy match cannot be found. If there is more than one potential match then both options will be printed out for the user to select.        
+    #     """
