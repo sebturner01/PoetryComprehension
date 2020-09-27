00e187059903df04c2431f78c16b10af351dea33
sebturner01 <sebturner01@gmail.com>
2020-09-27 12:14:51 -0600

    tiny final commit I promise

--- testing.py
+++ testing.py
@@ -3,26 +3,45 @@
 # This is the testing module for the Poetry comprehension package. 
 # As of (09/20) it will only consist of unit tests.
 
-import Parser *
+from Parser import readFromTextFile
+from parser import makePoem
+
+
+name = "TestResults.txt"
 
 if __name__ == "__main__":
 
+    #Creates a testing results file
+    with open(name, 'w') as f:
+        pass
+
+    poems = []
+    
     """Input Testing""" 
     #Should print None
-    poem = readFromTextFile("doesNotExist.txt")
-    print(poem)
+    poem1 = readFromTextFile("doesNotExist.txt")
+    poems.append(poem)
 
     #Should print None
     poem = readFromTextFile("")
-    print(poem)
+    poems.append(poem)
     
     poem = readFromTextFile("haiku.txt")
-    print(poem) 
+    poems.append(poem)
 
-    poem = readFromTextFile("GitaChp1")
+    poem = readFromTextFile("GitaChp1.txt")
+    poems.append(poem)
 
+   
+    """Testing Parser and Poem class Funcitons"""
     
-
+    with open(name, 'a') as f:
+        for p in poems:
+            newPoem = makePoem(p)
+            f.write(p)
+            for attr in newPoem.attributes:
+                f.write(attr)
+                
 
 
 
