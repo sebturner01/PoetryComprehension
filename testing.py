4267977d9bcaa2d4442a6c10005df12fa019748d
sebturner01 <sebturner01@gmail.com>
2020-09-29 09:59:34 -0600

    Refactored testing code

--- testing.py
+++ testing.py
@@ -2,7 +2,7 @@
 # Date: 09/26/20
 # This is the testing module for the Poetry comprehension package. 
 # As of (09/20) it will only consist of unit tests.
-
+import Poem
 import Parser
 import PoemAnalyzer
 
@@ -18,29 +18,34 @@ if __name__ == "__main__":
     poems = []
     
     """Input Testing""" 
-    #Should print None
+    #Testing with non-existentFile
     poem1 = readFromTextFile("doesNotExist.txt")
     poems.append(poem)
 
-    #Should print None
+    #Testing with empty string
     poem = readFromTextFile("")
     poems.append(poem)
     
+    #Testing with the Haiku
     poem = readFromTextFile("haiku.txt")
     poems.append(poem)
 
+    #Testing with a sonnet
+    poem = readFromTextFile("sonnet.txt")
+    poems.append(poem)
+    
+    #Testing with a longer text
     poem = readFromTextFile("GitaChp1.txt")
     poems.append(poem)
 
-   
     """Testing Parser, Poem class and PoemAnalyzer Funcitons"""
+
+    # Prints a poem's text and attributes to the testing file
+
     for p in poems:
         PoemAnalyzer.getPoemPhones(p)
         PoemAnalyzer.findAlliteration(p)
-
-    # Prints a poem's text and attributes to the testing file
-    with open(name, 'a') as f:
-        for p in poems:
+        with open(name, 'a') as f:
             newPoem = makePoem(p)
             f.write(p)
             for attr in newPoem.attributes:
